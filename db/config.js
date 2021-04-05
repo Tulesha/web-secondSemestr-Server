03bef8a7db9306a2c26913559945550109721951
Danil <tulesha_xochet_pit@mail.ru>
2021-04-05 13:35:06 +0300

    Implement node js package. Install other packages. Connect to db.

---
+++ db/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    urlDb: 'mongodb+srv://Tulesha:qwerty1234@cluster0.p5pwe.mongodb.net/Weather?retryWrites=true&w=majority'
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const conf = require("./config");
+const dbConf = require("./db/config");
+
+const app = express();
+const port = conf.port;
+const urlDb = dbConf.urlDb;
+
+app.use(cors());
+
+mongoose.set("useCreateIndex", true);
+
+mongoose.connect(urlDb, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false})
+        .then(() => {
+            console.log("Connection to db success");
+
+            app.listen(port, () => {
+                console.log("Server is started...")
+            })
+        })
+        .catch(console.log);
