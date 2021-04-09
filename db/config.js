8f06ce3294df217e723cd2d4bbd359ea547da918
Danil <tulesha_xochet_pit@mail.ru>
2021-04-09 14:13:42 +0300

    Try to connect db from heroku

--- db/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    urlDb: 'mongodb+srv://Tulesha:qwerty1234@cluster0.p5pwe.mongodb.net/Weather?retryWrites=true&w=majority'
-}
--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const conf = require("./config");
-const dbConf = require("./db/config");
 const routes = require("./routes/working");
 
 const app = express();
 const port = conf.port;
-const urlDb = dbConf.urlDb;
 const work = routes.working;
 
 app.use(cors());
 
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(urlDb, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false})
+mongoose.connect(process.env.MONGODB_URL, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false})
         .then((mongoose) => {
             console.log("Connection to db success");
 
